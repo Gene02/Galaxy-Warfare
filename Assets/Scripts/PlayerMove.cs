91397fa04f04fca5c2ba7647bc6e0b7dab6ff211
Gene02 <genesisparrilez1@gmail.com>
2022-11-23 13:36:48 -0600

    Cambios(Nuevo Script)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,41 +4,35 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed;
-    
+    public float speed = 10f;
+    public float padding = 0.8f;
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
     void Start()
     {
-        
+        FindBoundaries();
     }
 
-    
+    void FindBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        minX = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        maxX = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        minY = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        Vector2 direction = new Vector2(x, y).normalized;
+        float newXpos = Mathf.Clamp(transform.position.x + deltaX, minX, maxX);
+        float newYpos = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
 
-        Move(direction);
-    }
-    void Move(Vector2 direction)
-    {
-        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
-
-        max.x = max.x - 0.225f;
-        min.x = min.x + 0.225f;
-
-        max.y = max.y - 0.285f;
-        min.y = min.y + 0.285f;
-
-        Vector2 pos = transform.position;
-
-        pos += direction * speed * Time.deltaTime;
-
-        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
-        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
-
-        transform.position = pos;
+        transform.position = new Vector2(newXpos, newYpos);
     }
 }
